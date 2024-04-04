12fe10843e893710b97649756292e4e9b0240a16
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-04-04 01:03:19 -0700

    Removed unnessecary types

--- frontend/src/views/Project/MembersPage/components/MemberListTab/MemberRoleForm/SpecificPrivilegeSection.tsx
+++ frontend/src/views/Project/MembersPage/components/MemberListTab/MemberRoleForm/SpecificPrivilegeSection.tsx
@@ -83,7 +83,6 @@ export const SpecificPrivilegeSecretForm = ({
   onClose?: () => void;
 }) => {
   const { currentWorkspace } = useWorkspace();
-  const { membership: projectMembership } = useProjectPermission();
 
   const { popUp, handlePopUpOpen, handlePopUpToggle, handlePopUpClose } = usePopUp([
     "deletePrivilege",
@@ -269,17 +268,10 @@ export const SpecificPrivilegeSecretForm = ({
       conditions.secretPath = { $glob: data.secretPath };
     }
     await requestAccess.mutateAsync({
-      ...data,
       ...(data.temporaryAccess.isTemporary && {
-        temporaryAccessStartTime: data.temporaryAccess.temporaryAccessStartTime,
-        temporaryAccessEndTime: data.temporaryAccess.temporaryAccessEndTime,
-        temporaryRange: data.temporaryAccess.temporaryRange,
-        temporaryMode: "relative"
+        temporaryRange: data.temporaryAccess.temporaryRange
       }),
-      envSlug: data.environmentSlug,
-      secretPath: data.secretPath,
       projectSlug: currentWorkspace.slug,
-      projectMembershipId: projectMembership.id,
       isTemporary: data.temporaryAccess.isTemporary,
       permissions: actions
         .filter(({ allowed }) => allowed)
@@ -310,7 +302,7 @@ export const SpecificPrivilegeSecretForm = ({
     if (isExpired) return "Access expired";
     if (!temporaryAccessField?.isTemporary) return "Permanent";
 
-    if (exactTime) {
+    if (exactTime && !policies) {
       return `Until ${format(
         new Date(temporaryAccessField.temporaryAccessEndTime || ""),
         "yyyy-MM-dd HH:mm:ss"
